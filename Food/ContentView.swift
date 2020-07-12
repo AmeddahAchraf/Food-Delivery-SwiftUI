7ae41f792110fdc0f6eda96af5859a82cb5d61c7
Ameddah Achraf <fm_ameddah@esi.dz>
2020-07-12 14:25:52 +0100

    Initial Commit

---
+++ Food/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Food
+//
+//  Created by BqNqNNN on 7/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Food/FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodApp.swift
+//  Food
+//
+//  Created by BqNqNNN on 7/12/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
